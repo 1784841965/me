b9fed12cffa2c331a0551bc9a434275122aec265
whw <1784841965@qq.com>
2020-12-15 19:18:30 +0800

    github

--- ncov1/js/index.js
+++
@@ -1,9 +0,0 @@
-var tabbar = document.querySelectorAll('.tabbar>div:not([class="none"])');
-tabbar.forEach(function(item, index, lists) {
-    item.onclick = function() {
-        lists.forEach(function(item) {
-            item.style.color = 'black';
-        })
-        this.style.color = 'blue';
-    }
-})
